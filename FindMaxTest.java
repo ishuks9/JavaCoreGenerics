31878e1f174c8ab95b5e03a7ca90ae0493690c06
ishuks9 <ishuks9@gmail.com>
2021-07-18 14:54:12 +0530

    updated the uc2

--- FindMaxTest.java
+++ FindMaxTest.java
@@ -1,3 +1,5 @@
+
+
 public class FindMaxTest {
 
 	public Integer findMaxValue(Integer firstNumber, Integer secondNumber, Integer thirdNumber) {
@@ -11,13 +13,35 @@ public class FindMaxTest {
         printMax(max);
         return max;
     }
+    public Float findMaxValue1(Float firstNumber, Float secondNumber, Float thirdNumber) {
+        Float max = firstNumber;
+        if (secondNumber.compareTo(max) > 0) {
+            max = secondNumber;
+        }
+        if (thirdNumber.compareTo(max) > 0) {
+            max = thirdNumber;
+        }
+        printMax(max);
+        return max;
+    }
 
     private void printMax(Integer max) {
         System.out.println("Largest = " + max);
     }
+
+    private void printMax(Float max) {
+        System.out.println("Largest = " + max);
+    }
+
     
     public static void main(String[] args) {
+    	
 		FindMaxTest findmax= new FindMaxTest();
 		findmax.findMaxValue(10, 20, 30);
+		
+		FindMaxTest findmax1= new FindMaxTest();
+		findmax1.findMaxValue1(10.5f, 20.5f, 30.5f);
+
 	}
 }
+
