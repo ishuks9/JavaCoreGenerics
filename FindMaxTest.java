d61532c7dff0a328bf53bfd8ea112b7cac294196
ishuks9 <ishuks9@gmail.com>
2021-07-18 22:24:33 +0530

    updated the uc5

--- FindMaxTest.java
+++ FindMaxTest.java
@@ -1,43 +1,45 @@
+import java.util.Arrays;
+
 /*
  * Generic Class To Find Max Values
  */
 public class FindMaxTest<E extends Comparable<E>> {
-   
-    
-    
-   
-	 E i,j,k;
+    E[] values;
 
-    
+    @SafeVarargs
+    public FindMaxTest(E... values) {
+        this.values = values;
+    }
 
-    public FindMaxTest(E i, E j, E k) {
-		// TODO Auto-generated constructor stub
-    	 this.i = i;
-    	 this.j=j;
-    	 this.k=k;
-	}
-
-
-    //Method to Find Max Value
-    public <E extends Comparable<E>> E findMaxValue(E i, E j,E k) {
-    	E max=i;
-        if (j.compareTo(max)>0)  {
-            max=j;
-           
-        } if (k.compareTo(max)>0)
-        		{
-        	     max=k;
-        	    
-        		}
-          return max;
+    public E findMaxValue() throws FindMaxException {
+        if (values.length == 0) {
+            throw new FindMaxException("No Elements to Compare");
+        } else {
+            Arrays.sort(values);
+            printMax(values[values.length - 1]);
+            return values[values.length - 1];
         }
-    
+    }
+
+    /*
+     *Method to print Max Value
+     * @param is Generic Value
+     */
+    public void printMax(E value) {
+        System.out.println("Max Value = " + value);
+    }
+
      public static void main(String[] args) {
     	 
 			FindMaxTest findmax = new FindMaxTest("capgemini","tcs","cognizant");
 			
-			 findmax.findMaxValue("capgemini","tcs","cognizant");
-			 System.out.println(findmax);
+			 try {
+				findmax.findMaxValue();
+			} catch (FindMaxException e) {
+			
+				e.printStackTrace();
+			}
+			
 		}
 }
 
